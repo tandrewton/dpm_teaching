02086eb7ad36b33c6fa7ad2d1b3b821954262702
Andrew Ton <atatat123@gmail.com>
2022-11-15 10:38:08 -0500

    untested, but filling out skeleton of classes for teaching

--- src/dpm.cpp
+++ src/dpm.cpp
@@ -2,7 +2,82 @@
 
 // function definitions go here
 
-// determine if number is even
-bool isEven(int number){
-  return (number % 2 == 0);
+// Main constructor
+sp::sp(int n, int seed) {
+  // local variables
+  int d, i;
+
+  // print to console
+  cout << "** Instantiating sp object, NCELLS = " << n << ", seed = " << seed << " ..." << endl;
+
+  // main variables
+  NCELLS = n;
+  NDIM = 2;
+
+  // set scalars to default values
+  dt = 0.0;
+
+  k = 0.0;
+
+  // default boundary variables
+  L.resize(NDIM);
+  pbc.resize(NDIM);
+  for (d = 0; d < NDIM; d++) {
+    L[d] = 1.0;
+    pbc[d] = 1;
+  }
+
+  // seed random number generator
+  srand48(seed);
+}
+
+// destructor
+dpm::~dpm() {
+  // clear all private vectors
+  L.clear();
+  pbc.clear();
+  k.clear();
+  r.clear();
+  x.clear();
+  v.clear();
+  F.clear();
+  // add more as you go, but not important unless you're constantly constructing and destructing
+
+  if (posout.is_open())
+    posout.close();
+}
+
+// get vertex kinetic energy
+double sp::kineticEnergy() {
+  double K = 0;
+
+  for (int i = 0; i < NCELLS; i++)
+    K += v[i] * v[i];
+  K *= 0.5;
+
+  return K;
+}
+
+void sp::repulsiveForceUpdate() {
+  resetForcesAndEnergy();
+  repulsiveForces();
+}
+
+void sp::printConfiguration() {
+// check if pos object is open
+	if (!posout.is_open()) {
+		cerr << "** ERROR: in printConfiguration2D, posout is not open, but function call will try to use. Ending here." << endl;
+		exit(1);
+	}
+	else
+		cout << "** In printConfiguration2D, printing particle positions to file..." << endl;
+
+  // save box sizes
+	Lx = L.at(0);
+	Ly = L.at(1);
+
+	// print information starting information
+	posout << setw(w) << left << "NEWFR"
+		   << " " << endl;
+
 }
--- src/dpm.h
+++ src/dpm.h
@@ -1,9 +1,114 @@
 #ifndef DPM_H
 #define DPM_H
+/*
+
+        HEADER FILE FOR DPM CLASS
+
+                -- Stores vertex information for NCELLS with NV vertices each
+                -- Stores shape parameters + spring constants
+                -- Integrates equations of motion (NVE)
+                -- Prints data to files
+
+*/
+
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// global constants
+const double PI = 4.0 * atan(1.0);
 
 // function declarations and classes go here
+class sp {  // soft particle sp
+ protected:
+  // int scalars
+  int NCELLS;
 
-bool isEven(int number);
+  // number of dimensions
+  int NDIM;
 
+  // time step size
+  double dt;
+
+  // potential energy
+  double U;
+
+  // particle spring constants
+  double k;
+
+  // particle parameters
+  std::vector<double> r;
+
+
+  // boundary parameters
+  std::vector<double> L;
+  std::vector<bool> pbc;
+
+  // dynamical variables
+  std::vector<double> x;
+  std::vector<double> v;
+  std::vector<double> F;
+
+  // output objects
+  std::ofstream posout;
+
+ public:
+  // Constructors and Destructors
+  sp(int n, int seed);
+  sp(int n, int seed)
+      : sp(n, seed) {}
+  ~sp();
+
+  // -- G E T T E R S
+
+  // main ints
+  int getNCELLS() { return NCELLS; };
+  int getNDIM() { return NDIM; };
+
+  // force parameters
+  double getdt() { return dt; };
+  double getk() { return k; };
+
+  // boundary variables
+  double getL(int d) { return L.at(d); };
+  bool getpbc(int d) { return pbc.at(d); };
+
+  // calculate system properties
+  double packingFraction();
+  double kineticEnergy();
+
+  // Initialize particles (two dimensions)
+  void monodisperse();
+  void bidisperse(double smallfrac, double sizefrac);
+
+  void initializePositions(double phi0, double Ftol);
+
+  // editing & updating
+  void scaleParticleSizes(double scaleFactor);
+  void drawVelocities(double T);
+
+  // force definitions
+  void resetForcesAndEnergy();
+  void repulsiveForces();
+
+  // force updates
+  void repulsiveForceUpdate();
+
+  // simple integrators
+  void vertexFIRE2D(dpmMemFn forceCall, double Ftol, double dt0);
+  void vertexNVE2D(std::ofstream& enout, dpmMemFn forceCall, double T, double dt0, int NT, int NPRINTSKIP);
+
+  // protocols
+  void vertexCompress2Target(dpmMemFn forceCall, double Ftol, double dt0, double phi0Target, double dphi0);
+  void vertexJamming(dpmMemFn forceCall, double Ftol, double Ptol, double dt0, double dphi0, bool plotCompression);
+
+  // print vertex information to file
+  void printConfiguration();
+};
 
 #endif
